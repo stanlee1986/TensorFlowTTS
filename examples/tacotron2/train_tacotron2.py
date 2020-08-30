c64456f014dede848d987aaf0f42075c1c5c6413
stanlee1986 <51210436+stanlee1986@users.noreply.github.com>
2020-08-30 08:36:35 -0400

    Update train_tacotron2.py

--- examples/tacotron2/train_tacotron2.py
+++ examples/tacotron2/train_tacotron2.py
@@ -16,8 +16,9 @@
 import tensorflow as tf
 
 physical_devices = tf.config.list_physical_devices("GPU")
-for i in range(len(physical_devices)):
-    tf.config.experimental.set_memory_growth(physical_devices[i], True)
+if len(physical_devices) > 0:
+    for i in range(len(physical_devices)):
+        tf.config.experimental.set_memory_growth(physical_devices[i], True)
 
 import sys
 
